6eb2a9e26d4f88184286cc4a2dde10cc71e3eb84
Chae-JS <jschae02@khu.ac.kr>
2026-04-08 21:21:02 +0900

    fix(schema): add composite unique index to KeyPair
    (name, user) 복합 유니크 인덱스를 추가하여
    서로 다른 사용자가 동일한 이름의 키페어를 가질 수 있도록 허용.

--- internal/schema/keypair.go
+++ internal/schema/keypair.go
@@ -6,6 +6,7 @@ import (
 	"entgo.io/ent"
 	"entgo.io/ent/schema/edge"
 	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
 	"github.com/google/uuid"
 )
 
@@ -24,5 +25,11 @@ func (KeyPair) Fields() []ent.Field {
 func (KeyPair) Edges() []ent.Edge {
 	return []ent.Edge{
 		edge.From("user", User.Type).Ref("keypairs").Unique().Required(),
+	}
+}
+
+func (KeyPair) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("name").Edges("user").Unique(),
 	}
 }
